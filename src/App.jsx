a6e642277a44ec72f4b777335edd267d847d699b
Ajay <ajaykumawat9904@gmail.com>
2025-01-28 14:46:44 +0530

    integrated tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Welcome to BookMyService App</h1>
+      <h1>Welcome to <span className='text-red-700 font-bold'>BookMyService</span> App</h1>
     </>
   )
 }
